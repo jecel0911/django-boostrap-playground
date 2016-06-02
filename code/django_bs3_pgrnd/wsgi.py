4f33226ff5409b37861f7199b7196334c0879b4d
Jeremias Ceciliano <jecel0911@gmail.com>
2016-06-01 21:43:11 -0600

    adding base proyect and dependencies

---
+++ code/django_bs3_pgrnd/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for django_bs3_pgrnd project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_bs3_pgrnd.settings")
+
+application = get_wsgi_application()
